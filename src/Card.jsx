6ae7ea7c4c73efaa7c82b6bef6249c1eaaa6d91e
robbie <robbiekruszynski@gmail.com>
2020-01-06 11:19:10 -0800

    make rk-styles branch and start to style the field

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,44 +1,62 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   card: {
     minWidth: 275,
-    marginBottom: 15,
+    marginBottom: 15
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)"
   },
   title: {
-    fontSize: 14,
+    fontSize: 4
   },
   pos: {
-    marginBottom: 12,
+    marginBottom: 12
   },
   cardActions: {
-    flexDirection: 'row-reverse'
-  },
+    flexDirection: "row-reverse"
+  }
 });
 
-export default function SimpleCard({name, link}) {
-  const classes = useStyles();
+const cardStyle = makeStyles({
+  card: {
+    color: "black",
+    border: "1px solid grey",
+    marginTop: 15,
+    boxShadow: 10
+  },
+  button: {
+    backgroundColor: "red",
+    border: "5px solid grey"
+  }
+});
 
+export default function SimpleCard({ name, link }) {
+  const classes = useStyles();
+  const cardWork = cardStyle();
   return (
-    <Card className={classes.card}>
+    <Card className={(classes.card, cardWork.card)}>
       <CardContent>
-        <Typography gutterBottom>
-          {name}
-        </Typography>
+        <Typography gutterBottom>{name}</Typography>
       </CardContent>
       <CardActions className={classes.cardActions}>
-        <Button variant="contained" size="small" href={link} target="_blank">
+        <Button
+          className={cardWork.button}
+          variant="contained"
+          color="primary"
+          size="small"
+          href={link}
+          target="_blank"
+        >
           Learn more
         </Button>
       </CardActions>
